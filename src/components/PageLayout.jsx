5712f5907958ba9e028768e700833c4dddfd5c6c
Son Dao <udemydaohsn@gmail.com>
2023-02-09 09:11:49 -0700

    add toggle color mode button

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Center, Text } from "@chakra-ui/react";
 import AppHeader from "./AppHeader";
+import ToggleColorModeButton from "./ToggleColorModeButton";
 
 const PageLayout = ({ children }) => {
   return (
@@ -12,6 +13,7 @@ const PageLayout = ({ children }) => {
       <Center m="2rem 0">
         <Text>Copyright My Blog 2023</Text>
       </Center>
+      <ToggleColorModeButton />
     </>
   );
 };
