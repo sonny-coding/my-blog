c3582c113b178e56dd28c59411c6ae4aeb071675
Son Dao <udemydaohsn@gmail.com>
2023-03-03 22:43:33 -0700

    add list styling

--- src/components/MDX/MdxComponents.js
+++ src/components/MDX/MdxComponents.js
@@ -1,7 +1,8 @@
 import texts from "./texts";
 import embeds from "./embeds";
 import codes from "./codes";
+import lists from "./lists";
 
-const components = { ...texts, ...embeds, ...codes };
+const components = { ...texts, ...embeds, ...codes, ...lists };
 
 export default components;
