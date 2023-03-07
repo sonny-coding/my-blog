88a814d29d4cad5cc64e79a0220f64903be46f9f
Son Dao <udemydaohsn@gmail.com>
2023-03-07 09:55:04 -0700

    add a useSwr hook

--- src/hooks/useGetViews.js
+++ src/hooks/useGetViews.js
@@ -1,2 +1,18 @@
 import axios from "axios";
 import useSwr from "swr";
+
+const fetcher = async (url) => {
+  const {
+    data: { totalViews },
+  } = await axios.get(url);
+};
+
+const useGetViews = (customID, totalViews) => {
+  const url = `/api/views${customID}`;
+  const config = {
+    fallbackData: totalViews,
+  };
+  return useSwr(url, fetcher, config);
+};
+
+export default useGetViews;
